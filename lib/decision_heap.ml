200e380dd72d755147c75f32a3f4b4e8fd850399
cyberpink <cozyflame42+github@gmail.com>
2026-02-14 17:10:45 -0600

    optimize decision heap with some more inlining and unsafe array accesses

--- lib/decision_heap.ml
+++ lib/decision_heap.ml
@@ -16,20 +16,23 @@ let create nvars = {
   size = 0;
   inc = 1.0;
 }
-let swap t i j =
-  let v_i = t.heap.(i) in
-  let v_j = t.heap.(j) in
-  t.heap.(i) <- v_j;
-  t.heap.(j) <- v_i;
-  t.pos.(v_i) <- j;
-  t.pos.(v_j) <- i
 
 let (.%()) = Float.Array.unsafe_get
 let (.%()<-) = Float.Array.unsafe_set
-let higher_priority t i j =
-  let v_i = t.heap.(i) in
-  let v_j = t.heap.(j) in
-  t.activity.%(v_i) > t.activity.%(v_j)
+let (.$()) : int array -> int -> int = Array.unsafe_get
+let (.$()<-) : int array -> int -> int -> unit = Array.unsafe_set
+
+let[@inline] swap t i j =
+  let {heap; pos; _} = t in
+  let v_i = heap.$(i) in
+  let v_j = heap.$(j) in
+  heap.$(i) <- v_j;
+  heap.$(j) <- v_i;
+  pos.$(v_i) <- j;
+  pos.$(v_j) <- i
+
+let[@inline] higher_priority t i j =
+  t.activity.%(t.heap.$(i)) > t.activity.%(t.heap.$(j))
 
 let rec sift_up t i =
   if i > 0 then
@@ -40,26 +43,27 @@ let rec sift_up t i =
     end
 
 let rec sift_down t i =
+  let size = t.size in
   let left = 2 * i + 1 in
   let right = left + 1 in
   let largest =
-    if left < t.size && higher_priority t left i then left else i
+    if left < size && higher_priority t left i then left else i
   in
   let largest =
-    if right < t.size && higher_priority t right largest then right else largest
+    if right < size && higher_priority t right largest then right else largest
   in
   if largest <> i then begin
     swap t i largest;
     sift_down t largest
   end
 
-let contains t v = t.pos.(Var.to_int v) <> invalid_pos
+let contains t v = t.pos.$(Var.to_int v) <> invalid_pos
 let insert t v =
   let v' = Var.to_int v in
-  if t.pos.(v') = invalid_pos then begin
+  if t.pos.$(v') = invalid_pos then begin
     let i = t.size in
-    t.heap.(i) <- v';
-    t.pos.(v') <- i;
+    t.heap.$(i) <- v';
+    t.pos.$(v') <- i;
     t.size <- t.size + 1;
     sift_up t i
   end
@@ -67,14 +71,14 @@ let insert t v =
 let pop_max t =
   if t.size = 0 then None
   else
-    let root = t.heap.(0) in
+    let root = t.heap.$(0) in
     let last_idx = t.size - 1 in
-    let last_val = t.heap.(last_idx) in
+    let last_val = t.heap.$(last_idx) in
     (* set new root *)
-    t.heap.(0) <- last_val;
-    t.pos.(last_val) <- 0;
+    t.heap.$(0) <- last_val;
+    t.pos.$(last_val) <- 0;
     (* remove old root *)
-    t.pos.(root) <- invalid_pos;
+    t.pos.$(root) <- invalid_pos;
     t.size <- t.size - 1;
     if t.size > 1 then sift_down t 0;      
     Some (Var.of_int root)
@@ -84,15 +88,17 @@ let rescale_threshold = 1e100
 let rescale_factor = 1e-100
 let decay_inv = 1.0 /. 0.95
 let rescale t =
+  let activity = t.activity in
   t.inc <- t.inc *. rescale_factor;
-  for i = 0 to Float.Array.length t.activity - 1 do
-    t.activity.%(i) <- rescale_factor *. t.activity.%(i)
+  for i = 0 to Float.Array.length activity - 1 do
+    activity.%(i) <- rescale_factor *. activity.%(i)
   done
 
 let bump t v =
+  let { activity; _} = t in
   let v' = Var.to_int v in
-  t.activity.%(v') <- t.activity.%(v') +. t.inc;
-  if t.activity.%(v') > rescale_threshold then rescale t;
-  if contains t v then sift_up t t.pos.(v')
+  activity.%(v') <- activity.%(v') +. t.inc;
+  if activity.%(v') > rescale_threshold then rescale t;
+  if contains t v then sift_up t t.pos.$(v')
 
 let decay_activity t = t.inc <- t.inc *. decay_inv
